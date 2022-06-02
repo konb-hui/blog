aa68336aad4aca1797473e3e425d5c1d58648382
konb-hui <2581881495@qq.com>
2022-06-02 15:04:52 +0800

    Site updated: 2022-06-02 15:04:50

--- waifu/autoload.js
+++ waifu/autoload.js
@@ -33,8 +33,8 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://konb-hui.github.io/blog/waifu"
+			apiPath: "https://live2d.fghrsh.net/api/",
+			//cdnPath: "https://konb-hui.github.io/blog/waifu"
 		});
 	});
 }
